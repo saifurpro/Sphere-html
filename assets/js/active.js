8510ef940acf8f07b90fc039a0158f11b05592c8
Saifur Rahman <saifur703@gmail.com>
2020-10-12 20:54:32 +0600

    first commit

---
+++ assets/js/active.js
@@ -0,0 +1,44 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		$('.welcome-slider').owlCarousel({
+            items: 1,
+            loop: true,
+            dots: true,
+            nav: false,
+            navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+            autoplay: false,
+        });
+
+		$('.footer-top-carousel').owlCarousel({
+            items: 4,
+            margin: 30,
+            loop: true,
+            dots: false,
+            nav: true,
+            navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+            autoplay: false,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:false,
+                },
+                768:{
+                    items:3,
+                },
+                991:{
+                    items:4,
+                }
+            }
+        });
+
+	});
+	
+	
+	jQuery(window).on("load", function(){
+		
+	});
+	
+})(jQuery);
